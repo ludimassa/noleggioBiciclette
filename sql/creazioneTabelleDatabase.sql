8cc6c3b6aa355b3b2978f01453c0bdab1884bacf
ludimassa <46421917+ludimassa@users.noreply.github.com>
2019-03-18 14:05:03 +0100

    Update creazioneTabelleDatabase.sql

--- sql/creazioneTabelleDatabase.sql
+++ sql/creazioneTabelleDatabase.sql
@@ -1,51 +1,56 @@
-#############################################
-# Popolazione città
-INSERT INTO citta (`idCitta`, `nomeCitta`) VALUES (1, 'Boscoreale');
-INSERT INTO citta (`idCitta`, `nomeCitta`) VALUES (2, 'Torre del Greco');
-INSERT INTO citta (`idCitta`, `nomeCitta`) VALUES (3, 'Torre Annunziata');
+# Creare un database chiamato noleggiobici
+# Settare in my.ini lower_case_table_names = 2
+CREATE TABLE citta(
+  idCitta int PRIMARY KEY ,
+  nomeCitta varchar(20)
+);
+CREATE TABLE utenti(
+  idUtente char(8) PRIMARY KEY,
+  nomeUtente varchar(20),
+  cognome varchar(30),
+  codiceFiscale varchar(16),
+  dataNascita datetime,
+  via varchar(50),
+  idCitta int,
+  CONSTRAINT utente_citta_idCitta_fk FOREIGN KEY (idCitta) REFERENCES citta (idCitta) ON UPDATE CASCADE
+);
+CREATE TABLE telefoni(
+  idTelefono int PRIMARY KEY AUTO_INCREMENT,
+  numero varchar(12),
+  idUtente char(8),
+  CONSTRAINT telef_utente_idUtente_fk FOREIGN KEY (idUtente) REFERENCES utenti (idUtente) ON UPDATE CASCADE
+);
+CREATE TABLE biciclette(
+  idBicicletta bit(64) PRIMARY KEY,
+  modello varchar(30),
+  dataAcquisto datetime DEFAULT current_timestamp
+);
+CREATE TABLE noleggia(
+  idUtente char(8),
+  idBicicletta bit(64),
+  dataOraPrelievo datetime,
+  dataOraRiconsegna datetime,
+  PRIMARY KEY (idUtente, idBicicletta, dataOraPrelievo),
+  CONSTRAINT noleggia_biciclette_idBicicletta_fk FOREIGN KEY (idBicicletta) REFERENCES biciclette (idBicicletta) ON UPDATE CASCADE,
+  CONSTRAINT noleggia_utenti_idUtente_fk FOREIGN KEY (idUtente) REFERENCES utenti (idUtente) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE stazioniNoleggio(
+  idStazione int PRIMARY KEY AUTO_INCREMENT,
+  nomeStazione varchar(20)
+);
+CREATE TABLE slot(
+  idSlot int PRIMARY KEY AUTO_INCREMENT,
+  numero int,
+  idStazione int,
+  CONSTRAINT slot_stazioniNoleggio_idStazione_fk FOREIGN KEY (idStazione) REFERENCES stazioniNoleggio (idStazione) ON UPDATE CASCADE
+);
 
-#############################################
-# Popolazione utenti
-INSERT INTO utenti (`idUtente`, `nomeUtente`, `cognome`, `codiceFiscale`, `dataNascita`, `via`, `idCitta`) VALUES ('01256342', 'Luigi', 'di Massa', 'DMSLGU89P16I862G', '1988-03-06 21:41:30', 'Via Pasquale Ottavio', 1);
-INSERT INTO utenti (`idUtente`, `nomeUtente`, `cognome`, `codiceFiscale`, `dataNascita`, `via`, `idCitta`) VALUES ('02256342', 'Marco', 'Greco', 'ORLOTV87P67IG', '1978-03-06 21:41:30', 'Via Orlando Ottavio', 2);
-INSERT INTO utenti (`idUtente`, `nomeUtente`, `cognome`, `codiceFiscale`, `dataNascita`, `via`, `idCitta`) VALUES ('03256342', 'Luca', 'Cimaglia', 'LUCIM8589333', '1985-03-03 21:41:30', 'Via Napoli', 3);
-
-#############################################
-# Popolazione Biciclette
-INSERT INTO biciclette (`idBicicletta`, `modello`, `dataAcquisto`) VALUES (b'010101010', 'BMX', '2018-03-06 21:46:14');
-INSERT INTO biciclette (`idBicicletta`, `modello`, `dataAcquisto`) VALUES (b'111101010', 'BIANCHI', '2018-02-06 21:46:14');
-INSERT INTO biciclette (`idBicicletta`, `modello`, `dataAcquisto`) VALUES (b'110101010', 'BMX', '2018-01-06 21:46:14');
-INSERT INTO biciclette (`idBicicletta`, `modello`, `dataAcquisto`) VALUES (b'110101011', 'ALTAFINI', '2019-01-06 21:46:14');
-
-#############################################
-# Popolazione stazione di Noleggio
-INSERT INTO stazioniNoleggio (`idStazione`, `nomeStazione`) VALUES (1, 'BOSCOREALE BIKE 1');
-INSERT INTO stazioniNoleggio (`idStazione`, `nomeStazione`) VALUES (2, 'BOSCOREALE BIKE 2');
-INSERT INTO stazioniNoleggio (`idStazione`, `nomeStazione`) VALUES (3, 'TORREDELGRECO BIKE');
-#############################################
-# Popolazione slot
-# Popolazione slot prima stazione
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (1, 1, 1);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (2, 2, 1);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (3, 3, 1);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (4, 4, 1);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (5, 5, 1);
-# Popolazione slot seconda stazione
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (6, 1, 2);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (7, 2, 2);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (8, 3, 2);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (9, 4, 2);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (10, 5, 2);
-# Popolazione slot terza stazione
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (11, 1, 3);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (12, 2, 3);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (13, 3, 3);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (14, 4, 3);
-INSERT INTO slot (`idSlot`, `numero`, `idStazione`) VALUES (15, 5, 3);
-
-#############################################
-# Prima associazione biciclette
-INSERT INTO assBicicletteSlot (`idBicicletta`, `idSlot`, `dataIngresso`, `dataUscita`) VALUES (b'010101010',1,'2019-03-01 00:00:00',NULL);
-INSERT INTO assBicicletteSlot (`idBicicletta`, `idSlot`, `dataIngresso`, `dataUscita`) VALUES (b'111101010',2,'2019-03-01 00:00:00',NULL);
-INSERT INTO assBicicletteSlot (`idBicicletta`, `idSlot`, `dataIngresso`, `dataUscita`) VALUES (b'110101010',8,'2019-03-01 00:00:00',NULL);
-INSERT INTO assBicicletteSlot (`idBicicletta`, `idSlot`, `dataIngresso`, `dataUscita`) VALUES (b'110101011',11,'2019-03-01 00:00:00',NULL);
+CREATE TABLE assBicicletteSlot(
+  idBicicletta bit(64),
+  idSlot int,
+  dataIngresso datetime,
+  dataUscita datetime,
+  PRIMARY KEY (idBicicletta, idSlot, dataIngresso),
+  CONSTRAINT assBicicletteSlot_biciclette_idBicicletta_fk FOREIGN KEY (idBicicletta) REFERENCES biciclette (idBicicletta) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT assBicicletteSlot_slot_idSlot_fk FOREIGN KEY (idSlot) REFERENCES slot (idSlot)
+);
