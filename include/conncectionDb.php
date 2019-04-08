2a21d1754d7e91cc72cf8da724248d65ecd0a7fd
ludimassa <46421917+ludimassa@users.noreply.github.com>
2019-04-08 12:35:43 +0200

    Delete conncectionDb.php

--- include/conncectionDb.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Siccome si assume che il dbms mariadb lavora sulla porta standard non viene specificata la porta
-$host="127.0.0.1";
-$mysqli = new mysqli($host, 'root', '', 'noleggiobici');
-?>
